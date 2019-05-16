5789d6c04af44011cb2495a496da2b0fbf29694e
Ullas <me@ullas.ca>
2019-05-15 19:20:17 -0500

    Started unit testing with NUnit

--- iQ/iQ/Program.cs
+++ iQ/iQ/Program.cs
@@ -14,23 +14,12 @@ namespace iQ
             var Bob = "AS,QS,8S,6S,4S";
             var Sally = "4S,4H,3H,QC,8C";
 
-            var JoeHand = Joe.Split(',');
-            var BobHand = Bob.Split(',');
-            var SallyHand = Sally.Split(',');
-
-            Console.WriteLine(isFlush(JoeHand));
-            Console.WriteLine(isFlush(BobHand));
-            Console.WriteLine(isFlush(SallyHand));
+            var joe = new Player("Joe", Joe.Split(','));
+            var bob = new Player("Bob", Bob.Split(','));
+            var sally = new Player("Sally", Sally.Split(','));
 
 
             Console.ReadLine();
-        }
-
-        static void getCards(String hand)
-        {
-            Card[] cards = new Card[5];
-            foreach (String card in hand.Split(',')) ;
-                
         }
 
         static bool isFlush(String[] hand)
@@ -55,57 +44,120 @@ namespace iQ
         }
     }
 
-    public class Card
+    public class Player
     {
-        public int suit;
+        public String name;
+        public Card[] hand;
+
+        public Player(String name, String[] hand)
+        {
+            this.name = name;
+            this.hand = new Card[5];
+
+            for (int i=0; i<this.hand.Length; i++)
+            {
+                this.hand[i] = new Card(hand[i]);
+            }
+
+        }
+
+
+    }
+
+    public class Card : IComparable<Card>
+    {
+        public int suitRank;
         public int rank;
         private String card;
+        public String suitName;
+        public String rankName;
+
         public Card(String card)
         {
             this.card = card;
-            SetSuit(card[0]);
-            SetRank(card[1]);
+            SetRank(card[0]);
+            SetSuit(card[1]);
         }
 
-        public void SetSuit(Char s)
+        //Assuming that the 
+        public void SetRank(Char s)
         {
             switch(s)
             {
                 case 'A':
                     rank = 13;
+                    rankName = "Ace";
                     break;
                 case 'K':
                     rank = 12;
+                    rankName = "King";
                     break;
                 case 'Q':
                     rank = 11;
+                    rankName = "Queen";
                     break;
                 case 'J':
                     rank = 10;
+                    rankName = "Jack";
                     break;
                 default:
-                    rank = (int) Char.GetNumericValue(s);
+                    rank = (int) Char.GetNumericValue(s) - 1;
+                    rankName = Char.ToString(s);
                     break;
             }
         }
 
-        public void SetRank(Char r)
+        public void SetSuit(Char r)
         {
             switch (r)
             {
                 case 'D':
-                    rank = 1;
+                    suitRank = 1;
+                    suitName = "Diamonds";
                     break;
                 case 'C':
-                    rank = 2;
+                    suitRank = 2;
+                    suitName = "Clubs";
                     break;
                 case 'H':
-                    rank = 3;
+                    suitRank = 3;
+                    suitName = "Hearts";
                     break;
                 case 'S':
-                    rank = 4;
+                    suitRank = 4;
+                    suitName = "Spades";
                     break;
             }
+        }
+
+        public int CompareTo(Card other)
+        {
+            bool sameRank = this.rank == other.rank;
+
+            if (sameRank)
+            {
+                if (this.suitRank > other.suitRank)
+                {
+                    return -1;
+                } 
+                else if (this.suitRank < other.suitRank)
+                {
+                    return 1;
+                }
+            }
+            else
+            {
+                if (this.rank > other.rank)
+                {
+                    return -1;
+                } 
+                else if (this.rank < other.rank)
+                {
+                    return 1;
+                }
+            }
+
+            return 0;
         }
     }
 }
